f3d56c54a5b4b7321608690316ce194a0753d892
Carlos Silva <silvacj90@gmail.com>
2022-12-17 15:06:14 -0500

    javascript added to the navbar

---
+++ app.js
@@ -0,0 +1,47 @@
+const navTop = document.querySelector('#nav-top')
+const navbar = document.querySelector('.navbar')
+const h1 = document.querySelector('h1')
+const content = document.querySelector('.content')
+const brand = document.querySelector('.brand')
+let sticky = navbar.offsetTop;
+let h1fade = 0;
+
+
+let rgb = (y) => {
+
+    if (window.scrollY == 0) {
+        return false;
+    }
+    brand.style.color = `rgb( ${Math.floor(Math.random() * 250) + 1},  ${Math.floor(Math.random() * 250) + 1}, ${Math.floor(Math.random() * 250) + 1})`;
+
+    setTimeout(rgb, 1000)
+}
+rgb()
+
+window.onscroll = () => {
+
+    let scroll = window.scrollY / 50
+
+
+    navbar.style.backgroundColor = `rgba(${0}, ${0}, ${0}, ${scroll})`;
+
+    h1.style.color = `rgba(${250}, ${250}, ${250}, ${.2 / scroll})`
+
+
+
+
+
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+        content.style = "margin-top: 45px;";
+        brand.style.color = `rgba(${250}, ${250}, ${250}, ${1})`
+        rgb()
+
+
+    } else {
+        brand.style.color = `rgba(${250}, ${250}, ${250}, ${0})`
+        navbar.classList.remove("sticky");
+        content.style = "margin-top: 0;"
+
+    }
+}
